fbc136d232a0a315cbe0d06b57a28387bc9583ae
IgnacioPinedo <ignacio.b.pinedo@hotmail.com>
2019-10-25 20:23:00 -0300

    added UserEvent

---
+++ JoinApi/Code/Entities/Users/Event/UserEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+[Table("tb_UserEvent")]
+public class UserEvent
+{
+    [Key]
+    public int Id { get; private set; }
+    public int UserId { get; set; }
+    public int EventId { get; set; }
+}
