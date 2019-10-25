d2becbb18b46796b3c1215117ab528f6ad6c456a
IgnacioPinedo <ignacio.b.pinedo@hotmail.com>
2019-10-25 20:07:50 -0300

    Creating the classes

---
+++ JoinApi/Code/Entities/Events/Type/EventType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JoinApi.Code.Entities.Events.Type
+{
+    public class EventType
+    {
+    }
+}
