77b48b177cc24e17e50ce0098f206ec6dc8caaec
IgnacioPinedo <ignacio.b.pinedo@hotmail.com>
2019-10-29 08:28:48 -0300

    Added settings

---
+++ JoinApi/Code/Entities/Settings/SettingsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+public class SettingsController : ApiController
+{
+    [HttpGet]
+    [Route("On")]
+    public IHttpActionResult Get()
+    {
+        return Ok(new
+        {
+            success = true,
+            message = "The Api is working properly!"
+        });
+    }
+}
