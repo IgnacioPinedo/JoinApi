2b0c39e84f08c77957519fcfc542bd08f9679690
IgnacioPinedo <ignacio.b.pinedo@hotmail.com>
2019-11-01 10:44:58 -0300

    Added description

--- JoinApi/Code/Entities/Events/Event.cs
+++ JoinApi/Code/Entities/Events/Event.cs
@@ -16,6 +16,7 @@ public class Event
     public int TypeId { get; set; }
     public int AdministratorId { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
     public DateTime Date { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
